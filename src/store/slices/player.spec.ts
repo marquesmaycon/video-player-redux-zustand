d4ecce7aff19662cc564bb49dd19c7292fdd58fc
Maycon Marques <mayconmarquesh@gmail.com>
2025-10-14 00:56:33 -0300

    add vitest for player slice tests

---
+++ src/store/slices/player.spec.ts
@@ -0,0 +1,51 @@
+import { describe, expect, it } from "vitest"
+
+import { next, play, player, playerSlice } from "./player"
+
+describe("player slice", () => {
+	const initialState = playerSlice.getInitialState()
+
+	it("should be able to play a lesson", () => {
+		const state = player(initialState, play([1, 2]))
+
+		expect(state.currentModuleIndex).toEqual(1)
+		expect(state.currentLessonIndex).toEqual(2)
+	})
+
+	it("should be able to play next lesson automatically", () => {
+		const state = player(initialState, next())
+
+		expect(state.currentModuleIndex).toEqual(0)
+		expect(state.currentLessonIndex).toEqual(1)
+	})
+
+	it("should be able to play next module automatically", () => {
+		const state = player(
+			{
+				...initialState,
+				currentLessonIndex: initialState.course.modules[0].lessons.length - 1,
+			},
+			next(),
+		)
+
+		expect(state.currentModuleIndex).toEqual(1)
+		expect(state.currentLessonIndex).toEqual(0)
+	})
+
+	it("should not update module or lesson if there is no next lesson", () => {
+		const lastModuleIndex = initialState.course.modules.length - 1
+		const lastLessonIndex =
+			initialState.course.modules[lastModuleIndex].lessons.length - 1
+		const state = player(
+			{
+				...initialState,
+				currentModuleIndex: lastModuleIndex,
+				currentLessonIndex: lastLessonIndex,
+			},
+			next(),
+		)
+
+		expect(state.currentModuleIndex).toEqual(lastModuleIndex)
+		expect(state.currentLessonIndex).toEqual(lastLessonIndex)
+	})
+})
