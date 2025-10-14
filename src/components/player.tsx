4c530ab28b2c3b956b27f95a52a1712a4d733812
Maycon Marques <mayconmarquesh@gmail.com>
2025-10-14 20:13:17 -0300

    responsive design and scroll styles

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -23,7 +23,7 @@ export function Player() {
 	}, [currentLesson])
 
 	return (
-		<div className="flex items-center justify-center h-screen">
+		<div className="flex items-center justify-center h-screen px-4">
 			<div className="flex w-5xl flex-col gap-6">
 				<div className="flex items-center justify-between">
 					<Header />
@@ -36,12 +36,12 @@ export function Player() {
 					</button>
 				</div>
 
-				<main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow pr-80">
-					<div className="flex-1">
+				<main className="relative flex flex-col lg:flex-row overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow">
+					<div className="flex-1 flex">
 						<Video />
 					</div>
 
-					<aside className="w-80 absolute top-0 divide-y-2 divide-zinc-900 bottom-0 right-0 border-l border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
+					<aside className="max-h-[405px] divide-y-2 divide-zinc-900 border-l border-zinc-800 bg-zinc-900 overflow-y-scroll">
 						{modules?.map(({ id, title, lessons }, index) => (
 							<Module
 								key={id}
