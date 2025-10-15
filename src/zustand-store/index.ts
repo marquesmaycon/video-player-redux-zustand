094efb7622f5ae7b2ce759d97b796c42a13cdc73
Maycon Marques <mayconmarquesh@gmail.com>
2025-10-15 11:40:09 -0300

    zustand init

---
+++ src/zustand-store/index.ts
@@ -0,0 +1,62 @@
+import { create } from "zustand"
+
+import { api } from "../lib/axios"
+
+type Course = {
+	modules: Array<{
+		id: string
+		title: string
+		lessons: Array<{ id: string; title: string; duration: string }>
+	}>
+}
+
+export type PlayerState = {
+	course: Course | null
+	currentModuleIndex: number
+	currentLessonIndex: number
+	loading: boolean
+	load: () => Promise<void>
+	play: (indexes: [number, number]) => void
+	next: () => void
+}
+
+export const useStore = create<PlayerState>((set, get) => ({
+	course: null,
+	currentModuleIndex: 0,
+	currentLessonIndex: 0,
+	loading: false,
+	load: async () => {
+		set({ loading: true })
+
+		const { data } = await api.get<PlayerState["course"]>("/course")
+
+		set({ course: data, loading: false })
+	},
+	play: (indexes: [number, number]) => {
+		set({
+			currentModuleIndex: indexes[0],
+			currentLessonIndex: indexes[1],
+		})
+	},
+	next: () => {
+		const { currentModuleIndex, currentLessonIndex, course } = get()
+
+		const nextLessonIndex = currentLessonIndex + 1
+		const nextLesson =
+			course?.modules[currentModuleIndex].lessons[nextLessonIndex]
+
+		if (nextLesson) {
+			set({ currentLessonIndex: nextLessonIndex })
+		} else {
+			const nextModuleIndex = currentModuleIndex + 1
+			const nextModule = course?.modules[nextModuleIndex]
+
+			if (nextModule) {
+				set({
+					currentModuleIndex: nextModuleIndex,
+					currentLessonIndex: 0,
+				})
+			}
+		}
+	},
+}))
